aac9f4905ce9459eefec816836c0b267ada7f7e1
Abdulbassit Abdullahi <funabab@gmail.com>
2023-07-07 23:41:42 +0100

    init: project init

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    'postcss-import',
+    'tailwindcss',
+    ['tailwindcss/nesting', 'postcss-nesting'],
+    'postcss-hover-media-feature',
+    'autoprefixer',
+  ],
 }
